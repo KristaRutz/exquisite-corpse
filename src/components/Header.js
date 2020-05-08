4dcef97f2da8f454003774db3d5610c97086f44a
Krista Rutz <rutzkri000@gmail.com>
2020-05-08 10:57:27 -0700

    Add a navigation bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,46 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import NavBar from "react-bootstrap/NavBar";
+import { Navbar, Nav, NavDropdown, Form, FormControl } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <Container>
-      <NavBar className="dark">
-        <ButtonGroup>
-          <Button>Home</Button>
-          <Button>Sign In/Register</Button>
-        </ButtonGroup>
-      </NavBar>
-    </Container>
+    // <NavBar variant="dark" bg="dark">
+    //   <ButtonGroup>
+    //     <Link to="/loading">
+    //       <Button>Loading</Button>
+    //     </Link>
+    //     <Link to="/">
+    //       <Button>About</Button>
+    //     </Link>
+    //   </ButtonGroup>
+    // </NavBar>
+    <Navbar sticky="top" bg="dark" expand="lg" variant="dark">
+      <Navbar.Brand href="/">Exquisite Corpse</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/new">New</Nav.Link>
+          <Nav.Link href="/loading">Link</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
