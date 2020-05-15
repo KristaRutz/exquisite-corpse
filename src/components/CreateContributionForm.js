a33d91d4ad43578236262b410685f4922c480404
Krista Rutz <rutzkri000@gmail.com>
2020-05-15 16:54:56 -0700

    Add accordion to view previous fragments of story

--- src/components/CreateContributionForm.js
+++ src/components/CreateContributionForm.js
@@ -2,6 +2,8 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
 import PropTypes from "prop-types";
 import { useFirestore } from "react-redux-firebase";
 import firebase from "firebase/app";
@@ -26,26 +28,57 @@ function CreateContributionForm(props) {
 
   function addNewFragmentToFirestore(event) {
     event.preventDefault();
-    let placeholderId;
+    const author =
+      firebase.auth().currentUser != null
+        ? firebase.auth().currentUser.uid
+        : "anonymous";
     const entry = {
-      authorId:
-        firebase.auth().currentUser != null
-          ? firebase.auth().currentUser.uid
-          : "anonymous",
+      authorId: author,
       content: event.target.content.value,
       //timeSubmitted: db.FieldValue.serverTimestamp(),
     };
     console.log(entry);
     db.collection("projects")
       .doc(projectId)
-      .update({ fragments: [...project.fragments, entry] });
+      .update({
+        fragments: [...project.fragments, entry],
+        authors: project.authors.includes(author)
+          ? project.authors
+          : [...project.authors, author],
+      });
     onCreateContributionFormSubmission();
   }
 
   return (
     <Container>
-      <h1>Contribute to the story</h1>
+      <h1>
+        Contribute to <em>{project.title}</em>
+      </h1>
       <hr />
+      <Accordion defaultActiveKey="0">
+        {project.fragments.map((fragment, index) => {
+          if (index === project.fragments.length - 1) {
+            return (
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="0">
+                  Part {index + 1}, by {fragment.authorId}
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>{fragment.content}</Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            );
+          } else {
+            return (
+              <Card bg="light">
+                <Card.Header>
+                  Part {index + 1}, by {fragment.authorId}
+                </Card.Header>
+              </Card>
+            );
+          }
+        })}
+      </Accordion>
       <Form onSubmit={addNewFragmentToFirestore}>
         {/* <Form.Group>
           <Form.Label>Working Title</Form.Label>
@@ -65,6 +98,7 @@ function CreateContributionForm(props) {
           <Form.Control
             as="textarea"
             name="content"
+            rows="4"
             placeholder="Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted..."
           />
         </Form.Group>
