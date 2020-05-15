d91ba0c78573f6d8eec5fcf1eeffba9687a84e9c
Krista Rutz <rutzkri000@gmail.com>
2020-05-15 09:49:17 -0700

    Reassign project obj properties with no fragments

--- src/components/CreateProjectForm.js
+++ src/components/CreateProjectForm.js
@@ -11,28 +11,16 @@ function CreateProjectForm(props) {
 
   function addNewProjectToFirestore(event) {
     event.preventDefault();
-    let placeholderId;
-    const entry = {
-      //authorId: firebase.auth().currentUser.uid,
-      content: event.target.content.value,
-      timeSubmitted: db.FieldValue.serverTimestamp(),
-    };
-    // const post = async (doc) => {
-    //   const doc_ref = await db.collection(my_collection).add(doc)
-    //   return doc_ref.id
-    // }
-    console.log(entry);
-    db.collection("fragments")
-      .add(entry)
-      .then((docRef) => {
-        placeholderId = docRef.id;
-      });
+    //authorId: firebase.auth().currentUser.uid,
     const newProject = {
       title: event.target.title.value,
+      description: event.target.description.value,
       isPublished: false,
       characterLimit: 240,
+      contributionLimit: 6,
       timeCreated: db.FieldValue.serverTimestamp(),
-      fragments: [entry],
+      fragments: [],
+      authors: [],
     };
     console.log(newProject);
     db.collection("projects").add(newProject);
@@ -49,6 +37,7 @@ function CreateProjectForm(props) {
           <Form.Control
             type="text"
             name="title"
+            defaultValue="New Project"
             placeholder="Give your project a title"
           />
           <Form.Text className="text-muted">
@@ -58,14 +47,13 @@ function CreateProjectForm(props) {
         </Form.Group>
 
         <Form.Group>
-          <Form.Label>Content</Form.Label>
+          <Form.Label>Description</Form.Label>
           <Form.Control
             as="textarea"
-            name="content"
+            name="description"
             placeholder="Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted..."
           />
         </Form.Group>
-        <Form.Group controlId="formBasicCheckbox"></Form.Group>
         <Button variant="warning" type="submit">
           Create
         </Button>
