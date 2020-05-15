944f6fab826167c92367c861d563ddee50fab34b
Krista Rutz <rutzkri000@gmail.com>
2020-05-15 13:52:33 -0700

    Add ListItems with progress bar

--- src/components/Room.js
+++ src/components/Room.js
@@ -2,8 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import Container from "react-bootstrap/Container";
 import Media from "react-bootstrap/Media";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import ListGroup from "react-bootstrap/ListGroup";
 import { BsCollectionFill } from "react-icons/bs";
-import { useFirestore } from "react-redux-firebase";
+// import { useFirestore } from "react-redux-firebase";
 import { useSelector } from "react-redux";
 import { useFirestoreConnect, isLoaded, isEmpty } from "react-redux-firebase";
 
@@ -12,81 +14,24 @@ function Room(props) {
   useFirestoreConnect([{ collection: "projects" }]);
   const projects = useSelector((state) => state.firestore.ordered.projects);
 
-  //const db = useFirestore();
-
-  // db.get({ collection: "projects", doc: projectId }).then((project) => {
-  //   const currentProject = {
-  //     title: project.get("title"),
-  //     isPublished: project.get("isPublished"),
-  //     characterLimit: project.get("characterLimit"),
-  //     contributionLimit: project.get("contributionLimit"),
-  //     timeCreated: project.get("timeCreated"),
-  //     fragments: project.get("fragments"),
-  //     authors: project.get("authors"),
-  //     id: projectId,
-  //   };
-  // });
   if (isLoaded(projects)) {
     return (
       <Container>
-        <h1 className="display-2">Room</h1>
-        {projects.map((project) => (
-          <Media>
-            <BsCollectionFill style={{ size: 40, margin: "20px" }} />
-            <Media.Body>
-              <h5>{project.title}</h5>
-              <p>{project.description}</p>
-            </Media.Body>
-          </Media>
-        ))}
-        <Media onClick={() => props.onSelectRoomClick()}>
-          <img
-            width={64}
-            height={64}
-            className="align-self-start mr-3"
-            src="holder.js/64x64"
-            alt="Generic placeholder"
-          />
-          <Media.Body>
-            <h5>Media Heading</h5>
-            <p>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-              scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
-              in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
-              nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-            </p>
-
-            <p>
-              Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel
-              eu leo. Cum sociis natoque penatibus et magnis dis parturient
-              montes, nascetur ridiculus mus.
-            </p>
-          </Media.Body>
-        </Media>
-        {/* <Media onClick={() => props.onSelectRoomClick()}>
-          <img
-            width={64}
-            height={64}
-            className="align-self-start mr-3"
-            src="holder.js/64x64"
-            alt="Generic placeholder"
-          />
-          <Media.Body>
-            <h5>Media Heading</h5>
-            <p>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-              scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
-              in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
-              nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-            </p>
-
-            <p>
-              Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel
-              eu leo. Cum sociis natoque penatibus et magnis dis parturient
-              montes, nascetur ridiculus mus.
-            </p>
-          </Media.Body>
-        </Media> */}
+        <h1 className="display-2">...room</h1>
+        <ListGroup>
+          {projects.map((project) => (
+            <ListGroup.Item>
+              <Media>
+                <BsCollectionFill style={{ size: 40, margin: "20px" }} />
+                <Media.Body>
+                  <h5>{project.title}</h5>
+                  <p>{project.description}</p>
+                  <ProgressBar now={100 / project.fragments.length} />
+                </Media.Body>
+              </Media>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
       </Container>
     );
   } else {
