80d82e58e2f59915c49cb2ea5ef86163d6ca55de
Krista Rutz <rutzkri000@gmail.com>
2020-05-18 15:58:23 -0700

    Add ability to create a project from the room

--- src/components/Room.js
+++ src/components/Room.js
@@ -4,8 +4,13 @@ import Container from "react-bootstrap/Container";
 import Media from "react-bootstrap/Media";
 import ProgressBar from "react-bootstrap/ProgressBar";
 import ListGroup from "react-bootstrap/ListGroup";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Nav from "react-bootstrap/Nav";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
 import { BsCollectionFill } from "react-icons/bs";
 import firebase from "firebase/app";
 import { useFirestore } from "react-redux-firebase";
@@ -14,7 +19,10 @@ import { useSelector } from "react-redux";
 import { useFirestoreConnect, isLoaded, isEmpty } from "react-redux-firebase";
 import CreateContributionForm from "./CreateContributionForm";
 import ProjectDetails from "./ProjectDetails";
+import OngoingProjectsList from "./OngoingProjectsList";
+import PublishedProjectsList from "./PublishedProjectsList";
 import LoadingScreen from "./LoadingScreen";
+import CreateProjectForm from "./CreateProjectForm";
 
 function Room(props) {
   const { roomId, onSelectRoomClick } = props;
@@ -23,7 +31,8 @@ function Room(props) {
   const projects = useSelector((state) => state.firestore.ordered.projects);
   const [showModal, setShowModal] = useState(false);
   const [currentProject, setCurrentProject] = useState(null);
-  const VIEW_ROOM = "room view";
+  const VIEW_CREATE_A_PROJECT = "view create a project";
+  const VIEW_ROOM = "view room";
   const VIEW_ADD_CONTRIBUTION_FORM = "view add contribution form";
   const VIEW_PROJECT_DETAILS = "view project details";
   const views = [VIEW_ROOM, VIEW_ADD_CONTRIBUTION_FORM, VIEW_PROJECT_DETAILS];
@@ -32,6 +41,7 @@ function Room(props) {
   const currentUserId = firebase.auth().currentUser
     ? firebase.auth().currentUser.uid
     : "anonymous";
+  const [key, setKey] = useState("home");
 
   function handleProjectClick(project) {
     setCurrentProject(project);
@@ -49,7 +59,10 @@ function Room(props) {
     setShowModal(false);
     setCurrentView(views[1]);
   }
-
+  function handleCreateProjectFormSubmission() {
+    alert("project was successfully created!");
+    setCurrentView(views[0]);
+  }
   function handlePublishProject(project) {
     db.collection("projects").doc(project.id).update({ isPublished: true });
   }
@@ -78,126 +91,104 @@ function Room(props) {
         <>
           <Container>
             <h1 className="display-2">...room</h1>
-            <ListGroup>
-              {projects
-                .filter((project) => !project.isPublished)
-                .map((project) => (
-                  <ListGroup.Item key={project.id}>
-                    <Media>
-                      <div>
-                        <ListGroup>
-                          {project.contributionLimit >
-                          project.fragments.length ? (
-                            <ListGroup.Item action disabled variant="primary">
-                              Publish
-                            </ListGroup.Item>
-                          ) : (
-                            <ListGroup.Item
-                              variant="primary"
-                              action
-                              onClick={() => handlePublishProject(project)}
-                            >
-                              Publish
-                            </ListGroup.Item>
-                          )}
-                          {currentUserId === project.authorId ||
-                          project.authorId === "anonymous" ? (
-                            <ListGroup.Item
-                              variant="danger"
-                              action
-                              onClick={() => handleDeleteProject(project)}
-                            >
-                              Delete
-                            </ListGroup.Item>
-                          ) : (
-                            <ListGroup.Item action disabled variant="danger">
-                              Delete
-                            </ListGroup.Item>
-                          )}
-                        </ListGroup>
-                      </div>
-                      <Media.Body onClick={() => handleProjectClick(project)}>
-                        <Container>
-                          <h5>{project.title}</h5>
-                          <p>
-                            Created at{" "}
-                            {project.timeCreated.toDate().toLocaleTimeString()}
-                          </p>
-                          <p className="lead">{project.description}</p>
-                          <ProgressBar
-                            striped
-                            now={
-                              (100 / project.contributionLimit) *
-                              project.fragments.length
-                            }
-                          />
-                        </Container>
-                      </Media.Body>
-                    </Media>
-                  </ListGroup.Item>
-                ))}
-            </ListGroup>
+            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+              <Row>
+                <Col sm={3}>
+                  {/* <hr /> */}
 
-            <h2>Published projects</h2>
-            <ListGroup>
-              {projects
-                .filter((project) => project.isPublished)
-                .map((project) => (
-                  <ListGroup.Item key={project.id}>
-                    <Media>
-                      <div>
-                        <ListGroup>
-                          {project.contributionLimit >
-                          project.fragments.length ? (
-                            <ListGroup.Item action disabled variant="primary">
-                              Publish
-                            </ListGroup.Item>
-                          ) : (
-                            <ListGroup.Item
-                              variant="primary"
-                              action
-                              onClick={() => handlePublishProject(project)}
-                            >
-                              Publish
-                            </ListGroup.Item>
-                          )}
-                          {currentUserId === project.authorId ||
-                          project.authorId === "anonymous" ? (
-                            <ListGroup.Item
-                              variant="danger"
-                              action
-                              onClick={() => handleDeleteProject(project)}
-                            >
-                              Delete
-                            </ListGroup.Item>
-                          ) : (
-                            <ListGroup.Item action disabled variant="danger">
-                              Delete
-                            </ListGroup.Item>
-                          )}
-                        </ListGroup>
-                      </div>
-                      <Media.Body onClick={() => handleProjectClick(project)}>
-                        <Container>
-                          <h5>{project.title}</h5>
-                          <p>
-                            Created at{" "}
-                            {project.timeCreated.toDate().toLocaleTimeString()}
-                          </p>
-                          <p className="lead">{project.description}</p>
-                          <ProgressBar
-                            striped
-                            now={
-                              (100 / project.contributionLimit) *
-                              project.fragments.length
-                            }
-                          />
-                        </Container>
-                      </Media.Body>
-                    </Media>
-                  </ListGroup.Item>
-                ))}
-            </ListGroup>
+                  <Container>
+                    <p>Projects</p>
+
+                    <Nav variant="pills" className="flex-column">
+                      <Nav.Item>
+                        <Nav.Link eventKey={VIEW_CREATE_A_PROJECT}>
+                          Create a Project
+                        </Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Ongoing Projects</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">
+                          Published Projects
+                        </Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                  </Container>
+                  <hr />
+                  <Container>
+                    <p>Room Settings</p>
+                    <Nav variant="pills" className="flex-column">
+                      <Nav.Item>
+                        <Nav.Link eventKey="1">View Members</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="2">Leave Room</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                  </Container>
+                  <hr />
+                </Col>
+                <Col sm={9}>
+                  <Container>
+                    <Tab.Content>
+                      <Tab.Pane eventKey={VIEW_CREATE_A_PROJECT}>
+                        <CreateProjectForm
+                          onCreateProjectFormSubmission={
+                            handleCreateProjectFormSubmission
+                          }
+                        />
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="first">
+                        <h1>Header 1</h1>
+                        <OngoingProjectsList
+                          projects={projects}
+                          handleDeleteProject={handleDeleteProject}
+                          handleProjectClick={handleProjectClick}
+                          handlePublishProject={handlePublishProject}
+                        />
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <h1>Header 2</h1>
+                        <PublishedProjectsList
+                          projects={projects}
+                          handleDeleteProject={handleDeleteProject}
+                          handleProjectClick={handleProjectClick}
+                          handlePublishProject={handlePublishProject}
+                        />
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Container>
+                </Col>
+              </Row>
+            </Tab.Container>
+            {/* <Container>
+              <Tabs
+                id="controlled-tab-example"
+                activeKey={key}
+                onSelect={(k) => setKey(k)}
+              >
+                <Tab eventKey="home" title="Home">
+                  <OngoingProjectsList
+                    projects={projects}
+                    handleDeleteProject={handleDeleteProject}
+                    handleProjectClick={handleProjectClick}
+                    handlePublishProject={handlePublishProject}
+                  />
+                </Tab>
+                <Tab eventKey="profile" title="Profile">
+                  <PublishedProjectsList
+                    projects={projects}
+                    handleDeleteProject={handleDeleteProject}
+                    handleProjectClick={handleProjectClick}
+                    handlePublishProject={handlePublishProject}
+                  />
+                </Tab>
+                <Tab eventKey="contact" title="Contact">
+                  <h1>tab 3</h1>
+                </Tab>
+              </Tabs>
+            </Container> */}
           </Container>
 
           <Modal show={showModal} onHide={handleClose}>
