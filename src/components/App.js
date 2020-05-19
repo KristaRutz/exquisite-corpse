532eef97134251e19fd57b2424fc1c976ec15369
Krista Rutz <rutzkri000@gmail.com>
2020-05-19 16:28:10 -0700

    Create HOC for determining Auth loading in app

--- src/components/App.js
+++ src/components/App.js
@@ -2,56 +2,56 @@ import React from "react";
 import { connect } from "react-redux";
 import { useSelector } from "react-redux";
 import { useFirebase, isLoaded, isEmpty } from "react-redux-firebase";
-//import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import AboutPage from "./About";
 import Header from "./Header";
 import Loading from "./Loading";
-import {
-  BrowserRouter,
-  Switch,
-  Route,
-  matchPath,
-  useRouteMatch,
-  useParams,
-} from "react-router-dom";
+import { BrowserRouter, Switch, Route, useParams } from "react-router-dom";
 import ProjectControl from "./ProjectControl";
 import CreateRoomForm from "./CreateRoomForm";
-import SignInForm from "./LogInForm";
 import Room from "./Room";
 import AccountControl from "./AccountControl";
+import LoadingScreen from "./LoadingScreen";
 
 function App() {
-  const firebase = useFirebase();
-  const auth = useSelector((state) => state.firebase.auth);
+  //const firebase = useFirebase();
+  //const auth = useSelector((state) => state.firebase.auth);
+
+  function AuthIsLoaded({ children }) {
+    const auth = useSelector((state) => state.firebase.auth);
+    if (!isLoaded(auth)) return <LoadingScreen />;
+    return children;
+  }
   return (
     <BrowserRouter>
       <Header />
-      <Switch>
-        <Route path="/room/:id">
-          <RoomRouting />
-        </Route>
-        <Route path="/room">
-          <Room />
-        </Route>
-        <Route path="/loading">
-          <div className="App">
-            <Loading />
-          </div>
-        </Route>
-        <Route path="/new">
-          <CreateRoomForm />
-        </Route>
-        <Route path="/account">
-          <AccountControl />
-        </Route>
-        <Route path="/">
-          <Container fluid>
-            <AboutPage />
-          </Container>
-        </Route>
-      </Switch>
+      <AuthIsLoaded>
+        <Switch>
+          <Route path="/room/:id">
+            <RoomRouting />
+          </Route>
+          <Route path="/room">
+            <Room />
+          </Route>
+          <Route path="/loading">
+            <div className="App">
+              <Loading />
+            </div>
+          </Route>
+          <Route path="/new">
+            <CreateRoomForm />
+          </Route>
+          <Route path="/account">
+            <AccountControl />
+          </Route>
+          <Route path="/">
+            <Container fluid>
+              <AboutPage />
+            </Container>
+          </Route>
+        </Switch>
+      </AuthIsLoaded>
     </BrowserRouter>
   );
 }
