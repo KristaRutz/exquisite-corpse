4bdf623cf7fadf9e6252e7e1a151e21121599a32
Krista Rutz <rutzkri000@gmail.com>
2020-05-20 15:52:37 -0700

    Add image

--- src/components/AccountDetails.js
+++ src/components/AccountDetails.js
@@ -9,38 +9,59 @@ import { useState } from "react";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import FormGroup from "react-bootstrap/FormGroup";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import styles from "./styles/styles";
 
 function AccountDetails(props) {
   const { user, onLogOutClick } = props;
   const views = ["register", "login", "logged out", "account details"];
   const [currentView, setCurrentView] = useState(views[0]);
   const auth = firebase.auth();
+  const photoStyle = {
+    width: 208,
+    height: 208,
+    borderRadius: "0.25rem",
+  };
 
   if (!isLoaded(auth)) {
     return <LoadingScreen />;
   } else {
+    console.log(user);
     return (
       <Container>
-        <h1 className="display-2">My Account</h1>
-        <img src={user.photoUrl}></img>
-        <Form>
-          <FormGroup>
-            <Form.Label for="displayName">Display Name</Form.Label>
-            <FormControl
-              name="displayName"
-              value={`${user.displayName}`}
-              disabled
-            />
-          </FormGroup>
-          <FormGroup>
-            <Form.Label for="email">Email</Form.Label>
-            <FormControl name="email" value={`${user.email}`} disabled />
-          </FormGroup>
-          <p>{user.uid}</p>
-        </Form>
-        <Button onClick={onLogOutClick} variant="danger">
-          Sign Out
-        </Button>
+        <h1 className="display-3" style={styles.headerMargin}>
+          My Account
+        </h1>
+        <Row>
+          <Col md="auto">
+            <img src={`${user.photoURL}`} style={photoStyle}></img>
+          </Col>
+          <Col>
+            <Form>
+              <FormGroup>
+                <Form.Label for="displayName">Display Name</Form.Label>
+                <FormControl
+                  name="displayName"
+                  value={`${user.displayName}`}
+                  disabled
+                />
+              </FormGroup>
+              <FormGroup>
+                <Form.Label for="email">Email</Form.Label>
+                <FormControl name="email" value={`${user.email}`} disabled />
+              </FormGroup>
+              {/* <p>{user.uid}</p> */}
+            </Form>
+            <Button onClick={onLogOutClick} variant="danger">
+              Sign Out
+            </Button>
+          </Col>
+        </Row>
+        <br />
+        <hr />
+
+        <h3 style={styles.headerMargin}>My Rooms</h3>
       </Container>
     );
   }
