e14ff4920d2d3711a3f2a794e7fa16cce9c9ca8e
Krista Rutz <rutzkri000@gmail.com>
2020-05-20 10:48:47 -0700

    export headerMargin styles

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,6 +1,17 @@
 const styles = {
   headerMargin: {
-    marginTop: "1em",
+    marginTop: "0.75em",
+    marginBottom: "0.25em",
+  },
+  timestampPill: {
+    backgroundColor: "#EFEFEF",
+    borderRadius: "0.25rem",
+    // padding: 3,
+    // margin: "2px 6px 2px 6px",
+  },
+  navbar: {
+    marginTop: 100,
+    paddingBottom: 40,
   },
 };
 
