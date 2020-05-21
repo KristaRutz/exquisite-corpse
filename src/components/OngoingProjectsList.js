817fe4d5a9e56c7dbccbfef6baa1d32fa92822f2
Krista Rutz <rutzkri000@gmail.com>
2020-05-20 22:03:41 -0700

    Add alerts when no projects yet and headers

--- src/components/OngoingProjectsList.js
+++ src/components/OngoingProjectsList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import Alert from "react-bootstrap/Alert";
 import Container from "react-bootstrap/Container";
 import Media from "react-bootstrap/Media";
 import ProgressBar from "react-bootstrap/ProgressBar";
@@ -29,68 +29,73 @@ function OngoingProjectsList(props) {
   const currentUserId = firebase.auth().currentUser
     ? firebase.auth().currentUser.uid
     : "anonymous";
-  return (
-    <Container>
-      <ListGroup>
-        {projects
-          .filter((project) => !project.isPublished)
-          .map((project) => (
-            <ListGroup.Item key={project.id}>
-              <Media>
-                <div>
-                  <ListGroup>
-                    {project.contributionLimit > project.fragments.length ? (
-                      <ListGroup.Item action disabled variant="primary">
-                        Publish
-                      </ListGroup.Item>
-                    ) : (
-                      <ListGroup.Item
-                        variant="primary"
-                        action
-                        onClick={() => handlePublishProject(project)}
-                      >
-                        Publish
-                      </ListGroup.Item>
-                    )}
-                    {currentUserId === project.authorId ||
-                    project.authorId === "anonymous" ? (
-                      <ListGroup.Item
-                        variant="danger"
-                        action
-                        onClick={() => handleDeleteProject(project)}
-                      >
-                        Delete
-                      </ListGroup.Item>
-                    ) : (
-                      <ListGroup.Item action disabled variant="danger">
-                        Delete
-                      </ListGroup.Item>
-                    )}
-                  </ListGroup>
-                </div>
-                <Media.Body onClick={() => handleProjectClick(project)}>
-                  <Container>
-                    <h5>{project.title}</h5>
-                    <p>
-                      Created at{" "}
-                      {project.timeCreated.toDate().toLocaleTimeString()}
-                    </p>
-                    <p className="lead">{project.description}</p>
-                    <ProgressBar
-                      striped
-                      now={
-                        (100 / project.contributionLimit) *
-                        project.fragments.length
-                      }
-                    />
-                  </Container>
-                </Media.Body>
-              </Media>
-            </ListGroup.Item>
-          ))}
-      </ListGroup>
-    </Container>
-  );
+  if (projects.length < 1) {
+    return <Alert variant="primary">No ongoing projects right now.</Alert>;
+  } else {
+    return (
+      <Container>
+        <h1>In progress</h1>
+        <ListGroup>
+          {projects
+            .filter((project) => !project.isPublished)
+            .map((project) => (
+              <ListGroup.Item key={project.id}>
+                <Media>
+                  <div>
+                    <ListGroup>
+                      {project.contributionLimit > project.fragments.length ? (
+                        <ListGroup.Item action disabled variant="primary">
+                          Publish
+                        </ListGroup.Item>
+                      ) : (
+                        <ListGroup.Item
+                          variant="primary"
+                          action
+                          onClick={() => handlePublishProject(project)}
+                        >
+                          Publish
+                        </ListGroup.Item>
+                      )}
+                      {currentUserId === project.authorId ||
+                      project.authorId === "anonymous" ? (
+                        <ListGroup.Item
+                          variant="danger"
+                          action
+                          onClick={() => handleDeleteProject(project)}
+                        >
+                          Delete
+                        </ListGroup.Item>
+                      ) : (
+                        <ListGroup.Item action disabled variant="danger">
+                          Delete
+                        </ListGroup.Item>
+                      )}
+                    </ListGroup>
+                  </div>
+                  <Media.Body onClick={() => handleProjectClick(project)}>
+                    <Container>
+                      <h5>{project.title}</h5>
+                      <p>
+                        Created at{" "}
+                        {project.timeCreated.toDate().toLocaleTimeString()}
+                      </p>
+                      <p className="lead">{project.description}</p>
+                      <ProgressBar
+                        striped
+                        now={
+                          (100 / project.contributionLimit) *
+                          project.fragments.length
+                        }
+                      />
+                    </Container>
+                  </Media.Body>
+                </Media>
+              </ListGroup.Item>
+            ))}
+        </ListGroup>
+      </Container>
+    );
+  }
 }
 
 OngoingProjectsList.propTypes = {
