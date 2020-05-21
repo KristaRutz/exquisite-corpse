75efb5a7480d27fda8605822c28d26c5eadee310
Krista Rutz <rutzkri000@gmail.com>
2020-05-20 22:02:50 -0700

    Update mainpage nav

--- src/components/ProjectControlMenu.js
+++ src/components/ProjectControlMenu.js
@@ -2,21 +2,33 @@ import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import PropTypes from "prop-types";
+import Nav from "react-bootstrap/Nav";
 
 function ProjectControlMenu(props) {
-  const [hidden, setHidden] = useState(true);
-
   return (
     <Container fluid>
-      {hidden ? (
-        <h4>Menu</h4>
-      ) : (
-        <Button onClick={() => props.onCreateProjectClick()}>
-          New Project
-        </Button>
-      )}
       <br />
-      <Button onClick={() => setHidden(!hidden)}>Hide/Show</Button>
+      <Container>
+        <p>Contribute</p>
+        <Nav variant="pills" className="flex-column">
+          <Nav.Item>
+            <Nav.Link href="/new">Create a room</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/room">Enter the commons</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Container>
+      <hr />
+      <Container>
+        <p>Account</p>
+        <Nav variant="pills" className="flex-column">
+          <Nav.Item>
+            <Nav.Link eventKey="/account">Account Settings</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Container>
+      <hr />
     </Container>
   );
 }
