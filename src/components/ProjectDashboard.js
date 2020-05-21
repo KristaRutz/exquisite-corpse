d1787633c1b9de6a0845792fdef1ab9d2c59d9b0
Krista Rutz <rutzkri000@gmail.com>
2020-05-20 23:13:13 -0700

    display project details in dashboard

--- src/components/ProjectDashboard.js
+++ src/components/ProjectDashboard.js
@@ -1,18 +1,24 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import PropTypes from "prop-types";
+import ProjectDetails from "./ProjectDetails";
 
 function ProjectDashboard(props) {
-  const { project } = props;
+  const { project, onBackToGalleryClick } = props;
   return (
     <Container fluid>
-      <h1 className="display-4">{project.title}</h1>
+      {/* <h1 className="display-4">{project.title}</h1> */}
+      <ProjectDetails
+        project={project}
+        onBackToRoomClick={onBackToGalleryClick}
+      />
     </Container>
   );
 }
 
 ProjectDashboard.propTypes = {
   project: PropTypes.object,
+  onBackToGalleryClick: PropTypes.func,
 };
 
 export default ProjectDashboard;
